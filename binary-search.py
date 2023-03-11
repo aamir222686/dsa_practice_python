814698d252146c028247d059e779793fdd42adc4
Aamir Hussain <aamir222686@yahoo.com>
2023-03-11 12:51:29 +0530

    Linear and binary search

---
+++ binary-search.py
@@ -0,0 +1,33 @@
+def binary_search(list, target):
+    first = 0; 
+    last = len(list) - 1
+    
+    while first <= last:
+        midpoint = (first + last)//2
+
+        if target == list[midpoint]:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+    return None
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+def verify(index):
+    if index != None:
+        print("Target found in list at position : ", index)
+    else:
+        print("Target not found in list")
+
+
+result = binary_search(numbers, 5)
+verify(result)
+
+result = binary_search(numbers, 10)
+verify(result)
+
+result = binary_search(numbers, 1)
+verify(result)
---
+++ linear_search.py
@@ -0,0 +1,26 @@
+def linear_search(list, target):
+    """
+    Return position of the item if true else returns None
+    """
+
+    for i in range(len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+def verify(index):
+    if index != None:
+        print("Target found in list at position : ", index)
+    else:
+        print("Target not found in list")
+
+numbers = [1,2,3,4,5,6]
+
+result = linear_search(numbers, 5)
+verify(result)
+
+result = linear_search(numbers, 10)
+verify(result)
+
+result = linear_search(numbers, 1)
+verify(result)
